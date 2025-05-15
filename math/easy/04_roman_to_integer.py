ac5f58a5741f1c18a11b76164c07b77ce9ac5a4b
decordoba <danieldecordobagil@hotmail.com>
2025-05-16 01:16:19 +0200

    Minor fix

--- math/easy/04_roman_to_integer.py
+++ math/easy/04_roman_to_integer.py
@@ -110,3 +110,22 @@ class Solution:
                 prev_i = prev_x = prev_c = 0
         n += prev_i + 10 * prev_x + 100 * prev_c
         return n
+
+    def romanToInt_short(self, s: str) -> int:
+        roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000
+        }
+
+        total = 0
+        prev = 0
+
+        for char in reversed(s):
+            value = roman[char]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+            prev = value
+
+        return total
